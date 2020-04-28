5c385f6d0fc0878b4ed1fe4dc87d8093bb754dbd
Kevin Boston <kbos22@gmail.com>
2020-04-28 22:04:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,7 @@
 def turn_count(board)
-  i = 0
-  #board.each do |char|
-    while i < 9
-    if board[i] == "X"
-      x_count +=1
-      elsif board[i] == "O"
-      o_count +=1
-    end 
-    i +=1
-  end
-  t_count = x_count + o_count
+  turns = 0
+  board.each do |turns|
+     
 end
 
 def current_player(t_count)
